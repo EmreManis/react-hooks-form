43bfdff46f6d1654578a03165306da4b77421700
Emre Manış <emremanis@Emres-MacBook-Air.local>
2021-02-23 21:41:14 +0300

    css upgrade

--- src/components/ProfileForm.js
+++ src/components/ProfileForm.js
@@ -7,30 +7,30 @@ const ProfileForm = () => {
     return (
         <section> {/*form section */}
             <form className={classes.formCss}>
-                <div>
+                <div className={classes.divStyle}>
                     <label>First Name</label>
-                    <input type="text"/>
+                    <input className={classes.fillWidth} type="text"/>
                 </div>
-                <div>
+                <div className={classes.divStyle}>
                     <label>Last Name</label>
-                    <input type="text"/>
+                    <input className={classes.fillWidth} type="text"/>
                 </div>
-                <div>
+                <div className={classes.divStyle}>
                     <label>Email</label>
-                    <input type="email"/>
+                    <input className={classes.fillWidth} type="email"/>
                 </div>
-                <div>
+                <div className={classes.divStyle2}>
                     <label>Sex</label>
                     <input type="radio"/>
                     <label>Male</label>
                     <input type="radio"/>
                     <label>Female</label>
                 </div>
-                <div>
+                <div className={classes.divStyle}>
                     <label>Notes</label>
-                    <textarea rows="3" cols="50"></textarea>
+                    <textarea className={classes.fillWidth} rows="8"></textarea>
                 </div>
-                <div>
+                <div className={classes.inputCss}>
                     <input type="submit" value="Submit"/>
                 </div>
             </form>

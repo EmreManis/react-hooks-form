975e27ef0d821c5effaceb4e37fb0f5ee063fef4
Emre Manış <emremanis@Emres-MacBook-Air.local>
2021-02-24 22:46:22 +0300

    set up hooks

--- src/components/ProfileForm.js
+++ src/components/ProfileForm.js
@@ -9,26 +9,26 @@ const ProfileForm = () => {
             <form className={classes.formCss}>
                 <div className={classes.divStyle}>
                     <label>First Name</label>
-                    <input className={classes.fillWidth} type="text"/>
+                    <input className={classes.fillWidth} type="text" placeholder="Homer" required/>
                 </div>
                 <div className={classes.divStyle}>
                     <label>Last Name</label>
-                    <input className={classes.fillWidth} type="text"/>
+                    <input className={classes.fillWidth} type="text" placeholder="Simpson" required/>
                 </div>
                 <div className={classes.divStyle}>
                     <label>Email</label>
-                    <input className={classes.fillWidth} type="email"/>
+                    <input className={classes.fillWidth} type="email" placeholder="test@test.com" required/>
                 </div>
                 <div className={classes.divStyle2}>
                     <label>Sex</label>
-                    <input type="radio"/>
+                    <input type="radio" name="gender" required/>
                     <label>Male</label>
-                    <input type="radio"/>
+                    <input type="radio" name="gender"/>
                     <label>Female</label>
                 </div>
                 <div className={classes.divStyle}>
                     <label>Notes</label>
-                    <textarea className={classes.fillWidth} rows="8"></textarea>
+                    <textarea className={classes.fillWidth} rows="8" placeholder="Write a note"></textarea>
                 </div>
                 <div className={classes.inputCss}>
                     <input type="submit" value="Submit"/>
